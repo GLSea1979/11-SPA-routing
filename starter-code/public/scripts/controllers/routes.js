c79a193b15667f29a7b25c59a6603775241cd44d
Gary Lundgren <builtbygl@gmail.com>
2017-01-17 15:01:33 -0800

    finished todos

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,6 +1,8 @@
 'use strict';
 
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// done: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
-
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+page('/', articleController.init);
+page('/about', aboutController.init);
+// done: What function do you call to activate page.js? Fire it off now, to execute
+page();
